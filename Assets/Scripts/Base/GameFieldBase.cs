1cbf8df5de33eac41aa0c17ea37490f6b5e1ccc9
mitryushina <natalya.mitryushina@yandex.com>
2017-05-18 08:37:35 +0300

    created field

---
+++ Assets/Scripts/Base/GameFieldBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameFieldBase : MonoBehaviour {
+
+    protected readonly int _columnCount = 9;
+    protected readonly int _rowCount = 11;
+	protected readonly int _breakWallsCount;
+
+	public GameFieldBase()
+	{
+		_breakWallsCount = (_columnCount * _rowCount) / 4;
+	}
+
+	public GameFieldBase(int columnCount, int rowCount, int breakWallsCount){
+		_columnCount = columnCount;
+		_rowCount = rowCount;
+		_breakWallsCount = (_columnCount * _rowCount) / 4;
+	}
+
+    public abstract void GenerateField();
+
+}
